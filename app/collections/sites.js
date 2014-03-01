4caeda5149dbf08ce7f2c0f6c2db9df8a749452b
avinashega <avinash.ega@gmail.com>
2014-03-01 21:26:38 +0530

    objectId error

--- app/collections/sites.js
+++ app/collections/sites.js
@@ -1,10 +1,9 @@
-var ObjectID = require('mongoskin').ObjectID;
 
 module.exports = function (db) {
 
     db.bind('sites', {
     	getById: function(id, cb){
-    		this.findOne({_id:ObjectId.createFromHexString(id)}, cb);
+    		this.findOne({_id:id}, cb);
     	}
 
     });
