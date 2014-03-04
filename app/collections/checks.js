252fea7c6b1dcea38456b21c6163eab1d916f204
avinashega <avinash.ega@gmail.com>
2014-03-04 13:31:31 +0530

    responsive alerts

--- app/collections/checks.js
+++ app/collections/checks.js
@@ -2,7 +2,9 @@
 module.exports = function (db) {
 
     db.bind('checks', { 
-    	
+    	getChecks: function(id, count, cb){
+    		this.find({id:id}).sort({$natural:-1}).limit(count, cb);
+    	}
     });
 
 };
---
+++ app/services/checks.js
@@ -0,0 +1,17 @@
+var q = require('q');
+var checks = require('../interface').db().checks;
+module.exports={
+		averageResponseTime: function(id, count){
+			return q.nbind(checks.getChecks, checks)(id, count).then(function(checks){
+				console.log(checks);
+				if(!checks || checks.length == 0){
+					return 0;
+				}
+				var sum;
+				for(var i=0;i<checks.length;i++){
+					sum+=checks[i].response_time
+				}
+				return sum/checks.length;
+			});
+		}
+}
